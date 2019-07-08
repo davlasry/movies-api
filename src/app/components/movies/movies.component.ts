17a3aeab460447740261cb7783a9ead0f89d001d
Lasry David <lasry.david@gmail.com>
2019-07-08 23:41:40 +0300

    header html + inject moviesService

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie.model';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-movies',
@@ -6,10 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies.component.scss']
 })
 export class MoviesComponent implements OnInit {
+  movies: Movie[];
 
-  constructor() { }
+  constructor(private moviesService: MoviesService) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
