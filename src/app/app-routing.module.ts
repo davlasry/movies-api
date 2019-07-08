788f573ace31e2bf08160ad8dde80f229ca8f96a
Lasry David <lasry.david@gmail.com>
2019-07-08 23:34:58 +0300

    import Material Angular + set main style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: AppComponent
+    path: 'movies',
+    component: MoviesComponent
   },
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: '',
+    redirectTo: '/movies',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/movies',
+    pathMatch: 'full'
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AboutComponent, MoviesComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
